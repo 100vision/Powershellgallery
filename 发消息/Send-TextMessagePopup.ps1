8c945a6d414c5585a22e4d6d7d64530844430e27
100vision <lin.tixiang@gmail.com>
2021-06-01 11:57:13 +0800

    add a few helpful scripts.

--- 发消息/Send-TextMessagePopup.ps1
+++ 发消息/Send-TextMessagePopup.ps1
@@ -1,5 +1,37 @@
-﻿$Computers="s2208","s3195","s1051","s3326","s3146","s1489","s1684","s2900"
+﻿$Computers="s0830","s0632"
 
 $msg="你好，这里是资讯部林提祥。你的电脑已经完成了杀毒软件的升级，清你尽快完成一次电脑重启。如有疑问，请电话130-630-84890"
 
 Invoke-WmiMethod -path win32_process -name Create -ArgumentList "msg * $msg" -ComputerName $Computers
+
+
+
+
+
+#################
+
+$Computers="s0830","s0632","s0998","g010"
+
+$msg="你好，这里是资讯部林提祥。你的电脑已经完成了补丁KB4474419的安装，清你尽快完成一次电脑重启。如有疑问，请电话130-630-84890"
+
+Invoke-WmiMethod -path win32_process -name Create -ArgumentList "msg * $msg" -ComputerName $Computers
+
+
+###########
+
+while(1)
+{
+    if (!(Test-NetConnection -ComputerName s3787).PingSucceed)
+    {
+        write-host " Computer reboot in progress"
+        return
+    }
+
+$msg="你好，这里是资讯部林提祥。你的电脑已经完成了补丁KB4474419的安装，清你尽快完成一次电脑重启。如有疑问，请电话130-630-84890"
+
+Invoke-WmiMethod -path win32_process -name Create -ArgumentList "msg * $msg" -ComputerName s3787
+
+Start-Sleep -Seconds 5
+
+
+    }
